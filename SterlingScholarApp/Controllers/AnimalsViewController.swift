d2d053f5c60b87d29d79825c17bc9d3f2b8fd817
Amber Bullough <amber@bertbullough.com>
2017-11-20 09:44:43 -0700

    Added

---
+++ SterlingScholarApp/Controllers/AnimalsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AnimalsViewController.swift
+//  SterlingScholarApp
+//
+//  Created by Bullough, Amber on 11/17/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class AnimalsViewController: UIViewController
+{
+    
+}
